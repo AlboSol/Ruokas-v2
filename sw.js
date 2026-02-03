a92a87c11f2d0a08648dc097ea68998deba2ade7
AlboSol <ff9jkhjmmw@privaterelay.appleid.com>
2026-02-03 20:40:06 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="ruokasi-v3-2-4-2";
+const CACHE="ruokasi-v3-2-4-3";
 const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.png"];
 self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
